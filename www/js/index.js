6863c0daba653f7766091b2a17cbc478bcedbff9
patricio <parango@tvemart.com>
2014-06-19 15:39:27 -0300

    two

--- www/js/index.js
+++ www/js/index.js
@@ -66,7 +66,8 @@ $(document).on('deviceready', function() {
             client_id: '150881333908-1ar412eou7ovegc9brhkuhjde4kr5d44.apps.googleusercontent.com',
             client_secret: 'ZG_u5iJYAnTjL3u72lxQEpQr',
             redirect_uri: 'http://localhost',
-            scope: 'https://www.googleapis.com/auth/plus.login'
+            //scope: 'https://www.googleapis.com/auth/plus.login'
+            scope: 'https://www.googleapis.com/auth/plus.profile.emails.read'
         }).done(function(data) {
             $loginStatus.html('Access Token: ' + data.access_token);
             //ocultar boton
