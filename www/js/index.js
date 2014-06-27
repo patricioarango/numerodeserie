a9ec8374c693decf982bf841e2445aae39dbacca
patricio <parango@tvemart.com>
2014-06-27 13:20:41 -0300

    hi

--- www/js/index.js
+++ www/js/index.js
@@ -122,9 +122,11 @@ $(document).on('deviceready', function() {
             }*/
            //acá hay que hacer una llamada tipo "me" porque ya logueado va a traer la info de la persona 
            //gapi.client.load('plus','v1', loadProfile); 
-           $.post('https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token='+data.access_token, {}, function(data) {
+           var url = 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token='+data.access_token;
+           window.location.href=url;
+           /*$.post(url, {}, function(data) {
                alert(data.name);
-           },"json");
+           },"json");*/
            
         }).fail(function(data) {
             $loginStatus.html(data.error);
