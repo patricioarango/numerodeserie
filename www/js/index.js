35b0d4856a83dcfb55b268291fe972292cb42527
patricio <parango@tvemart.com>
2014-07-03 12:20:40 -0300

    two

--- www/js/index.js
+++ www/js/index.js
@@ -190,7 +190,7 @@ function pedir_autenticacion() {
                 //alert(data23.given_name);
                 //alert("id: "+ data23.id + "nom: " + data23.given_name + "ape: " + data23.family_name + "email: " + data23.email + "foto: " + data23.picture);
                 insertar_usuario(data23.id,data23.given_name,data23.family_name,data23.email,data23.picture);
-                 window.localStorage.setItem("permiso_otorgado","1");
+                 window.localStorage.setItem("permiso_otorgado","2");
                  window.location.href = 'dashboard.html';
             },"json");
         }).fail(function(data) {
@@ -203,7 +203,7 @@ $(document).on('deviceready', function() {
     //creamos la db y las tablas
     var permiso = window.localStorage.getItem("permiso_otorgado");
     //var permiso = 0;
-    if (permiso=="1") {
+    if (permiso=="2") {
         window.location.href = 'dashboard.html';
     }
     else {
