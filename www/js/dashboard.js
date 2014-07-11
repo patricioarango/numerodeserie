58d042598a281bf29f0ba813362267c48628832b
patricio <parango@tvemart.com>
2014-07-11 10:32:10 -0300

    two

--- www/js/dashboard.js
+++ www/js/dashboard.js
@@ -1,8 +1,7 @@
-function traer_usuario() {
+function traer_usuario_local() {
 	$("#nombre").append(localStorage.usuario_nombre + " " + localStorage.usuario_apellido);
 	$("#email").append(localStorage.usuario_email);
 	$("#foto").append(localStorage.usuario_imagen);
-	alert(localStorage.usuario_nombre);
 }
 //Query the database
 function queryDB_u_g(tx) {
@@ -17,9 +16,23 @@ function querySuccess_u_g(tx, results) {
 	    alert(results.rows.item(i).firstName);
 	}
 }
-
+function meter_usuario() {
+	var db = window.openDatabase("Seriesmarker", "1.0", "Seriesmarker", 100 * 1024);
+    db.transaction(insertar_usuario, error2, success2);
+}
+function insertar_usuario(tx) {
+    var query = "insert into usuario (id,firstName,lastName,email,image) values ('" + localStorage.usuario_id + "','" + localStorage.usuario_nombre + "','" + localStorage.usuario_apellido + "','" + localStorage.usuario_email + "','" + localStorage.usuario_imagen + "')";                     
+    tx.executeSql(query);
+}
+function error2(){
+        alert("data is not inserted");
+}
+function success2(){
+        alert("data is succesfully inserted");
+}
 //init
 $(document).on('deviceready', function() {
-		traer_usuario();
+		traer_usuario_local();
+		meter_usuario();
 });
 
